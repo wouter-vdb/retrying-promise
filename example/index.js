2d5f2ef51a2b7937995bbeacdb0c9100cba78120
Wouter Van den Broeck <wouter.vandenbroeck@iminds.be>
2016-05-27 14:56:38 +0200

    Reformatting: using 2 instead of 4 spaces for indents and using arrow functions where appropriate.

--- example/index.js
+++ example/index.js
@@ -4,35 +4,47 @@ const retryingPromise = require('../lib/index');
 
 console.log(Promise.resolve(123).then((v) => console.log(v)));
 
-Promise.resolve().then(function () {
+Promise.resolve()
+  .then(() => {
     console.log('# Simply resolve:');
     return retryingPromise((resolve) => resolve('ok'));
-}).then((result) => {
+  })
+  .then(result => {
     console.log('  - result:', result);
-}).then(() => {
+  })
+  .then(() => {
     console.log('# Simply reject:');
     return retryingPromise((resolve, retry, reject) => reject('nok'));
-}).catch((err) => {
-    console.log('  - err:', err);
-}).then(() => {
+  })
+  .catch(error => {
+    console.log('  - err:', error);
+  })
+  .then(() => {
     console.log('# Resolve on third attempt:');
     var attempts = 0;
-    return retryingPromise({ minTimeout: 250, factor: 1 }, (resolve, retry, reject) => {
-        attempts++;
-        console.log('  - attempts:', attempts);
-        if (attempts == 3) { resolve(123); }
-        else { retry(); }
+    return retryingPromise({minTimeout: 250, factor: 1}, (resolve, retry, reject) => {
+      attempts++;
+      console.log('  - attempts:', attempts);
+      if (attempts == 3) {
+        resolve(123);
+      }
+      else {
+        retry();
+      }
     });
-}).then((result) => {
+  })
+  .then(result => {
     console.log('  - result:', result)
-}).then(() => {
+  })
+  .then(() => {
     console.log('# Reject after 3 attempts:');
     var attempts = 0;
-    return retryingPromise({retries: 3, minTimeout: 250, factor: 1 }, (resolve, retry, reject) => {
-        attempts++;
-        console.log('  - attempts:', attempts);
-        retry('nok');
+    return retryingPromise({retries: 3, minTimeout: 250, factor: 1}, (resolve, retry, reject) => {
+      attempts++;
+      console.log('  - attempts:', attempts);
+      retry('nok');
     });
-}).catch((err) => {
-    console.log('  - err:', err)
-});
+  })
+  .catch(error => {
+    console.log('  - err:', error)
+  });
--- test/index.js
+++ test/index.js
@@ -1,63 +1,70 @@
-const chai            = require('chai');
-const chaiAsPromised  = require("chai-as-promised");
+const chai = require('chai');
+const chaiAsPromised = require("chai-as-promised");
 
 const retryingPromise = require('../lib/index');
 
 const should = chai.should();
 chai.use(chaiAsPromised);
 
+describe('retrying-promise', () => {
 
-describe('retrying-promise', function () {
+  describe('resolve', () => {
+    it('should simply resolve', () => {
+      return retryingPromise((resolve, retry, reject) => {
+        setTimeout(() => resolve('ok'), 250);
+      }).should.eventually.equal('ok');
+    })
+  });
 
-    describe('resolve', function () {
-        it('should simply resolve', function () {
-            return retryingPromise(function (resolve) {
-                setTimeout(() => resolve('ok'), 250);
-            }).should.eventually.equal('ok');
-        })
+  describe('resolve', () => {
+    it('should resolve on third attempt', (done) => {
+      var retries = 0;
+      retryingPromise({minTimeout: 200, factor: 1}, (resolve, retry, reject) => {
+        retries++;
+        if (retries == 3) {
+          resolve('ok');
+        }
+        else {
+          retry('nok');
+        }
+      }).then(
+        (result) => {
+          result.should.equal('ok');
+          retries.should.equal(3);
+          done();
+        },
+        (err) => {
+          throw new Error('rejected');
+        }
+      );
     });
+  });
 
-    describe('resolve', function () {
-        it('should resolve on third attempt', function (done) {
-            var retries = 0;
-            retryingPromise({ minTimeout: 200, factor: 1 }, function (resolve, retry) {
-                retries++;
-                if (retries == 3) { resolve('ok'); }
-                else { retry('nok'); }
-            }).then(
-                (result) => {
-                    result.should.equal('ok');
-                    retries.should.equal(3);
-                    done();
-                },
-                (err) => { throw new Error('rejected'); }
-            );
-        });
-    });
+  describe('reject', () => {
+    it('should simply reject', () => {
+      return retryingPromise((resolve, retry, reject) => {
+        setTimeout(() => reject('nok'), 250);
+      }).should.eventually.rejectedWith('nok');
+    })
+  });
 
-    describe('reject', function () {
-        it('should simply reject', function () {
-            return retryingPromise(function (resolve, retry, reject) {
-                setTimeout(() => reject('nok'), 250);
-            }).should.eventually.rejectedWith('nok');
-        })
+  describe('retries', () => {
+    it('should do three retries and then reject', (done) => {
+      var retries = 0;
+      retryingPromise({retries: 3, minTimeout: 200, factor: 1}, (resolve, retry, reject) => {
+        retries++;
+        retry('nok');
+      }).then(
+        (result) => {
+          throw new Error('resolved');
+        },
+        (err) => {
+          err.should.equal('nok');
+          retries.should.equal(3);
+          done();
+        }
+      );
     });
-
-    describe('retries', function () {
-        it('should do three retries and then reject', function (done) {
-            var retries = 0;
-            retryingPromise({ retries: 3, minTimeout: 200, factor: 1 }, function (resolve, retry) {
-                retries++;
-                retry('nok');
-            }).then(
-                (result) => { throw new Error('resolved'); },
-                (err) => {
-                    err.should.equal('nok');
-                    retries.should.equal(3);
-                    done();
-                }
-            );
-        });
-    });
+  });
 
 });
