4469cc1fb14edd82bf138e63a17c369b6a7f92bc
Wouter Van den Broeck <wouter.vandenbroeck@iminds.be>
2016-05-27 15:33:09 +0200

    Multiple calls of retry in one retry cycle are now ignored.

--- test/index.js
+++ test/index.js
@@ -13,30 +13,23 @@ describe('retrying-promise', () => {
       return retryingPromise((resolve, retry, reject) => {
         setTimeout(() => resolve('ok'), 250);
       }).should.eventually.equal('ok');
-    })
+    });
   });
 
   describe('resolve', () => {
-    it('should resolve on third attempt', (done) => {
-      var retries = 0;
+    it('should resolve on third attempt', done => {
+      var trial = 0;
       retryingPromise({minTimeout: 200, factor: 1}, (resolve, retry, reject) => {
-        retries++;
-        if (retries == 3) {
-          resolve('ok');
-        }
-        else {
-          retry('nok');
-        }
-      }).then(
-        (result) => {
+        trial++;
+        if (trial == 3) { resolve('ok'); }
+        else { retry('nok'); }
+      })
+        .then(result => {
           result.should.equal('ok');
-          retries.should.equal(3);
+          trial.should.equal(3);
           done();
-        },
-        (err) => {
-          throw new Error('rejected');
-        }
-      );
+        })
+        .catch(error => { throw new Error('should not be rejected'); });
     });
   });
 
@@ -45,25 +38,51 @@ describe('retrying-promise', () => {
       return retryingPromise((resolve, retry, reject) => {
         setTimeout(() => reject('nok'), 250);
       }).should.eventually.rejectedWith('nok');
-    })
+    });
   });
 
   describe('retries', () => {
-    it('should do three retries and then reject', (done) => {
-      var retries = 0;
+    it('should do three retries and then reject', done => {
+      var trial = 0;
       retryingPromise({retries: 3, minTimeout: 200, factor: 1}, (resolve, retry, reject) => {
-        retries++;
+        trial++;
         retry('nok');
-      }).then(
-        (result) => {
-          throw new Error('resolved');
-        },
-        (err) => {
-          err.should.equal('nok');
-          retries.should.equal(3);
+      })
+        .then(result => {
+          throw new Error('should not resolve');
+        })
+        .catch(error => {
+          error.should.equal('nok');
+          trial.should.equal(3);
           done();
+        });
+    });
+  });
+
+  describe('retries', () => {
+    it('should not allow multiple retries in one cycle', done => {
+      var trial = 0;
+      retryingPromise({retries: 3, minTimeout: 200, factor: 1}, (resolve, retry, reject) => {
+        trial++;
+        if (trial == 1) {
+          retry('this retry should be accepted');
+          retry('this retry should be ignored');
         }
-      );
+        else if (trial == 2) {
+          resolve('ok');
+        }
+        else {
+          throw new Error('should not retry more than once');
+        }
+      })
+        .then(result => {
+          result.should.equal('ok');
+          trial.should.equal(2);
+          done();
+        })
+        .catch(error => {
+          throw new Error('should not be rejected');
+        });
     });
   });
 
